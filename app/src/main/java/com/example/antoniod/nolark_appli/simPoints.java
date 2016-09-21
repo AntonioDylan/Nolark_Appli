8c0a58540d9c00066c7c925dd7fd8ebd28eacb24
Dasporal <rahmane.a@sio.lan>
2016-09-21 15:16:21 +0200

    création de la classe simPoints qui comportera les méthodes pour la simulation

--- app/src/main/java/com/example/antoniod/nolark_appli/simPoints.java
+++
@@ -1,7 +0,0 @@
-package com.example.antoniod.nolark_appli;
-
-/**
- * Created by dasporal on 21/09/2016.
- */
-public class simPoints {
-}
---
+++ app/src/main/java/simPoints.java
@@ -0,0 +1,5 @@
+/**
+ * Created by rahmane.a on 21/09/2016.
+ */
+public class simPoints {
+}
