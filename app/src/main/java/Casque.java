bb7a1ab931787f5decfb351bb1e6827909be9c42
Dylkiller <dylan_83000@hotmail.fr>
2016-09-21 14:27:12 +0200

    déclaration des attributs

--- app/src/main/java/Casque.java
+++ app/src/main/java/Casque.java
@@ -2,5 +2,17 @@
  * Created by antonio.d on 21/09/2016.
  */
 public class Casque {
-    private 
+
+    private string type;
+    private BufferedImage img ; //= ImageIO.read(new File("my.png")
+    private float prix;
+    private string marque;
+    private string reference;
+    private boolean dispo;
+    private float note;
+
+    public Casque(string p_type, BufferedImage p_img, float p_prix, string p_marque, string reference, boolean p_dispo, ) {
+
+    }
+
 }
