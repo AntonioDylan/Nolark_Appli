028b923efc493bcc9656658ba1f8e638d8139fd7
Bunjigai21 <romain.nicoli83@gmail.com>
2016-09-26 11:15:32 +0200

    Simulateur sans choix de sexe

--- app/src/main/java/com/example/antoniod/nolark_appli/Accueil.java
+++ app/src/main/java/com/example/antoniod/nolark_appli/Accueil.java
@@ -1,7 +1,10 @@
 package com.example.antoniod.nolark_appli;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Accueil extends AppCompatActivity {
 
@@ -9,5 +12,6 @@ public class Accueil extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
+
     }
 }
--- app/src/main/java/com/example/antoniod/nolark_appli/simAlcool.java
+++ app/src/main/java/com/example/antoniod/nolark_appli/simAlcool.java
@@ -1,13 +1,101 @@
 package com.example.antoniod.nolark_appli;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.content.DialogInterface;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.RadioButton;
+        import android.widget.RadioGroup;
+        import android.widget.TextView;
+
+        import java.util.function.ToIntFunction;
 
 public class simAlcool extends AppCompatActivity {
+    final TextView txtAlcool = (TextView) findViewById(R.id.txtAlcool);
+    final TextView txtAmende = (TextView) findViewById(R.id.txtAmende);
+    final TextView txtSanction = (TextView) findViewById(R.id.txtSanction);
+
+    private double vPoids;
+    private int uniteAlcool =10;
+    private double coefDiffuH = 0.7;
+    private double coefDiffuF = 0.6;
+    private double txAlcool;
+    private int vVerres;
+    private char sexe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sim_alcool);
+        final Button btnCalcul = (Button) findViewById(R.id.btnCalcul);
+        btnCalcul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                amende();
+            }
+        });
+    }
+
+    public simAlcool(){
+        final EditText nbVerres = (EditText) findViewById(R.id.nbVerres);
+        final EditText nbPoids = (EditText) findViewById(R.id.nbPoids);
+        final RadioGroup RG = (RadioGroup) findViewById(R.id.RG);
+        this.vVerres=Integer.parseInt(nbVerres.getText().toString());
+        this.vPoids=Integer.parseInt(nbPoids.getText().toString());
+
+    }
+
+
+    /*public char getSexe(){
+        if( RG.getSelection() == rbHomme) {
+            char sexe ='H';
+        } else if (RG.getSelection() == rbFemme) {
+            char sexe ='F';
+        }
+        return sexe;
+    }*/
+
+    public double getAlcoolemie(int vVerres, double vPoids, char sexe, int uniteAlcool, double coefDiffuF, double coefDiffuH) {
+       if (sexe == 'H') {
+            txAlcool = (vVerres * uniteAlcool) / (vPoids * 0.6);
+        } else if (sexe == 'F') {
+            txAlcool = (vVerres * uniteAlcool) / (vPoids * coefDiffuF);
+        }
+        return txAlcool;
+    }
+
+    public void amende(){
+        sexe='H';
+        double txAlcool = getAlcoolemie(vVerres, vPoids, sexe, uniteAlcool, coefDiffuF, coefDiffuH);
+        String amende;
+        String sanction;
+        if (vVerres>0){
+            if(txAlcool >= 0.5) {
+                if (txAlcool <= 0.8){
+                    sanction = "+ 6 points + suspension 3 ans";
+                    amende = "90 €/135 €/375 €";
+                    txtAmende.setText("Amende minorée/Forfaitaire/Majorée: " + amende);
+                    txtSanction.setText("Retrait :" + sanction);
+                }
+                else{
+                    sanction = "+ 6 points + 2 ans de prisons + suspension de 3 ans + stage de sensibilisation";
+                    amende = "4 500 €";
+                    txtAmende.setText(amende);
+                    txtSanction.setText("Retrait :" + sanction);
+                }
+            }
+            else{
+                txtAmende.setText("Vous pouvez conduire mais soyez vigilant, vous avez bu!");
+                txtSanction.setText("");
+                }
+        }
+        else{
+            txtAmende.setText("C’est bien, quand on conduit, on ne boit pas !");
+            txtSanction.setText("");
+        }
+        txtAlcool.setText("Votre taux d'alcoolémie est de : " + txAlcool +" g/l de sang");
     }
 }
+
