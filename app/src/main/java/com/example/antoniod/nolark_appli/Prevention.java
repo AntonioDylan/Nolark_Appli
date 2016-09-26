45a4c6fc4c8a56375b01eb9ee5ce506814932712
Feronax <guillyan.chapput@gmail.com>
2016-09-26 10:35:28 +0200

    stuff

---
+++ app/src/main/java/com/example/antoniod/nolark_appli/Prevention.java
@@ -0,0 +1,12 @@
+package com.example.antoniod.nolark_appli;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Prevention extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
