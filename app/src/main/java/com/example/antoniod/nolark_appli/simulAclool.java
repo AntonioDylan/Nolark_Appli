1e572767ee416b5046f8191c7a24b758d69bc8ca
Feronax <guillyan.chapput@gmail.com>
2016-09-30 11:52:01 +0200

    Création simulateur

---
+++ app/src/main/java/com/example/antoniod/nolark_appli/simulAclool.java
@@ -0,0 +1,13 @@
+package com.example.antoniod.nolark_appli;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class simulAclool extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simul_aclool);
+    }
+}
